0fc0dc51ffb568c05d2baf2c0fb01ab80a4340a9
GenericPirate <julien.baars@gul.ag>
2024-02-24 21:30:44 +0100

    Add example of tile state

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Tile from './Tile'
 
 function App() {
-  const initialTileData = [
-    { imageState: viteLogo },
-    { imageState: viteLogo },
-    { imageState: viteLogo },
-    { imageState: viteLogo },
-    { imageState: viteLogo },
-  ]
-  const [tileData, setTileData] = useState(initialTileData);
+  let initialTiles = [
+    'react', 'vite', 'react', 'vite'
+  ];
 
   const handleTileClick = (clickedIndex) => {
-    setTileData((prevTileData) => {
-      return prevTileData.map((tile, index) => {
-        if (index == clickedIndex) {
-          return { ...tile, imageState: tile.imageState === viteLogo ? reactLogo : viteLogo}
-        } else if (isAdjacent(index, clickedIndex)) {
-          return { ...tile, imageState: tile.imageState === viteLogo ? reactLogo : viteLogo}
-        }
-        return tile;
-      })
-    });
+    console.log('Tile clicked', clickedIndex);
   }
 
   const isAdjacent = (i1, i2) => Math.abs((i1 - i2)) == 1
 
   return (
     <div>
-      {tileData.map((tile, index) => (
+      {initialTiles.map((tile, index) => (
         <Tile
           key={index}
           imageState={tile.imageState}
           onClick={() => handleTileClick(index)}
+          displayedImage='react'
         />
       ))}
     </div>
--- src/Tile.jsx
+++ src/Tile.jsx
@@ -1,11 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-const Tile = ({ imageState, onClick }) => {
+import ReactLogo from './assets/react.svg'
+import ViteLogo from '/vite.svg'
+
+/**
+ * @param {'react'|'vite'} props.displayedImage
+ */
+const Tile = ({ imageState, onClick, displayedImage }) => {
+  const [image, setImage] = useState(null);
+  useEffect(() => {
+    switch (displayedImage) {
+      case 'react':
+        setImage(ReactLogo);
+        break;
+      case 'vite':
+        setImage(ViteLogo);
+        break;
+      default:
+        setImage(null);
+    }
+  }, [displayedImage]);
+
+  const swapLogo = () => {
+    if (image === ReactLogo) {
+      setImage(ViteLogo);
+    } else if (image === ViteLogo) {
+      setImage(ReactLogo);
+    }
+  };
+
   return (
-    <div onClick={onClick}>
-      <img src={`${imageState}`} alt={`Image`} />
-    </div>
+    <img onClick={()=>{onClick();swapLogo();}} src={image} alt="Displayed" />
   );
+};
+
+Tile.propTypes = {
+  displayedImage: PropTypes.oneOf(['react', 'vite']),
+};
+
+Tile.defaultProps = {
+  displayedImage: 'vite',
 };
 
 export default Tile;
